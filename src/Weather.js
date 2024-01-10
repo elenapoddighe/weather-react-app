7189c76c35270f97659d2a9fbe4fb3da136ac331
elenapoddighe <podd.elena@gmail.com>
2024-01-10 17:19:29 +0100

    Created temporary app

--- src/Weather.js
+++ src/Weather.js
@@ -1,27 +1,98 @@
 import React from "react";
-import axios from "axios";
-import { BallTriangle } from "react-loader-spinner";
+import "./Weather.css";
 
-export default function Weather(props) {
-  function handleResponse(response) {
-    alert(
-      `The weather in ${response.data.name} is ${response.data.main.temp}C`
-    );
-  }
-  let apiKey = "e0a5a97de9a0b7a951e9d154a8f9bad8";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(handleResponse);
+export default function Weather() {
+  let weatherData = {
+    city: "New York",
+    time: "Monday, 13:23",
+    humidity: "81%",
+    speed: "6km/h",
+    temperature: "15",
+    longitude: "2.1774322",
+    latitude: "41.3828939",
+    feels: "9°",
+    description: "scattered clouds",
+    suggestion: "Don't forget the sunglasses!😎",
+    imgUrl:
+      "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/109/707/original/sunny.png?1704713621",
+  };
 
   return (
-    <BallTriangle
-      height={100}
-      width={100}
-      radius={5}
-      color="#4fa94d"
-      ariaLabel="ball-triangle-loading"
-      wrapperStyle={{}}
-      wrapperClass=""
-      visible={true}
-    />
+    <div className="weather">
+      <header>
+        <form className="search-form">
+          <input
+            type="search"
+            placeholder="Enter a city..."
+            required
+            className="search-form-input"
+          />
+
+          <input type="submit" value="Search" className="search-form-button" />
+        </form>
+      </header>
+
+      <main>
+        <h1 className="city">{weatherData.city}</h1>
+
+        <div className="data">
+          <div className="weather-info-left">
+            <p>
+              <span className="time">{weatherData.time}</span>
+              <br />
+              Humidity:{" "}
+              <strong className="humidity">{weatherData.humidity}</strong>
+              <br />
+              Wind: <strong className="speed">{weatherData.speed}</strong>
+            </p>
+          </div>
+          <div className="temperature-container">
+            <div className="icon">
+              <img src={weatherData.imgUrl} alt={weatherData.decription} />
+            </div>
+            <div className="temperature">{weatherData.temperature}</div>
+            <div className="unit">°</div>
+          </div>
+          <div className="weather-info-right">
+            <p>
+              Long:{" "}
+              <strong className="longitude">{weatherData.longitude}</strong>
+              <br />
+              Lat: <strong className="latitude">{weatherData.latitude}</strong>
+              <br />
+              Feels like:{" "}
+              <strong className="feels-like">{weatherData.feels}</strong>
+            </p>
+          </div>
+        </div>
+        <div className="description-suggestion-container">
+          <p className="description">{weatherData.description}</p>
+          <p className="suggestion">{weatherData.suggestion}</p>
+        </div>
+      </main>
+
+      <footer>
+        <p>
+          Coded by
+          <a href="https://github.com/elenapoddighe" target="_blank">
+            Elena Poddighe
+          </a>
+          , open-sourced on{" "}
+          <a
+            href="https://github.com/elenapoddighe/my-awesome-weather-app"
+            target="_blank"
+          >
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://elenas-awesome-weather-app.netlify.app/"
+            target="_blank"
+          >
+            Netlify
+          </a>
+        </p>
+      </footer>
+    </div>
   );
 }
